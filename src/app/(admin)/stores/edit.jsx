31c9da3b17b8bc47411bea105b8e182205d9b920
koshtijatin26 <koshtijatin26@gmail.com>
2025-06-27 11:54:25 +0530

    general

--- src/app/(admin)/stores/edit.jsx
+++ src/app/(admin)/stores/edit.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import PageMetaData from '@/components/PageTitle'
 import { API_URL_ADMIN } from '../../../context/constants'
